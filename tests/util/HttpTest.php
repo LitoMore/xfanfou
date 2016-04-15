68b786e44f9e5f05ddec44c3e93364adc004f144
Hotcha <hotcha.py@gmail.com>
2016-04-16 03:18:20 +0800

    封装 HTTP 方法.

---
+++ tests/util/HttpTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use Util\Network\Http;
+
+class HttpTest extends TestCase
+{
+    const PORT = 1984;
+    const URL  = 'http://localhost:' . self::PORT;
+
+    private $php_pid = null;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->php_pid = shell_exec('php -S localhost:' . self::PORT . ' -t ' .
+            __DIR__ . '/server > /dev/null 2>&1 & echo $!');
+        sleep(1);
+    }
+
+    public function makeHttp()
+    {
+        return new Http;
+    }
+
+    public function testRequestBody()
+    {
+        $r = $this->makeHttp()->post(self::URL . '/echo.php');
+        $this->assertEquals(200, $r->statusCode);
+        $this->assertEquals('', $r->body);
+        $this->assertEquals('', $r);
+
+        $r = $this->makeHttp()->post(self::URL . '/echo.php', ['foo' => 'bar']);
+        $this->assertEquals(200, $r->statusCode);
+        $this->assertEquals('foo=bar', $r->body);
+        $this->assertEquals('foo=bar', $r);
+
+        $r = $this->makeHttp()->get(self::URL . '/echo.php', ['foo' => 'bar']);
+        $this->assertEquals('{"foo":"bar"}', $r);
+
+        $r = $this->makeHttp()->get(self::URL . '/echo.php', 'foo=bar');
+        $this->assertEquals('{"foo":"bar"}', $r);
+
+        $r = $this->makeHttp()->get(self::URL . '/echo.php?a=b', ['foo' => 'bar']);
+        $this->assertEquals('{"a":"b","foo":"bar"}', $r);
+
+        $r = $this->makeHttp()->post(self::URL . '/404.php');
+        $this->assertEquals(404, $r->statusCode);
+    }
+
+    public function testFailedResponse()
+    {
+        $r = $this->makeHttp()->post(self::URL . '/failure.php', ['foo' => 'bar']);
+        $this->assertEquals(500, $r->statusCode);
+        $this->assertEquals('Failure', $r);
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        if ($this->php_pid !== null) {
+            shell_exec('kill -9 ' . $this->php_pid);
+        }
+    }
+}
--- util/Network/Http.php
+++ util/Network/Http.php
@@ -2,7 +2,86 @@
 
 namespace Util\Network;
 
+/**
+ * Curl wrapper class
+ */
 class Http
 {
+    /**
+     * Curl handle
+     * @var resource
+     */
+    protected $ch;
+
+    /**
+     * Makes an HTTP request of GET or POST
+     * @param  string $method
+     * @param  string $url
+     * @param  array $data
+     * @return Response
+     */
+    public function request($method, $url, $data = null)
+    {
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_HEADER, true);
+
+        if ($method === 'post') {
+            curl_setopt($this->ch, CURLOPT_POST, 1);
+        }
+
+        if (!empty($data)) {
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        }
+
+        $response = curl_exec($this->ch);
+        if ($response === false) {
+            $errmsg = curl_error($this->ch);
+            $errno  = curl_errno($this->ch);
+            curl_close($this->ch);
+            throw new Exception($errmsg, $errno);
+        }
+
+        $statusCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+        $headerSize = curl_getinfo($this->ch, CURLINFO_HEADER_SIZE);
+        $info       = curl_getinfo($this->ch);
+
+        // return string or false
+        $body = substr($response, $headerSize);
+
+        $response = new Response($body, $statusCode, $info);
+
+        curl_close($this->ch);
+
+        return $response;
+    }
+
+    /**
+     * HTTP GET request
+     * @param  string $url
+     * @param  array|string $args
+     * @return Response
+     */
+    public function get($url, $args = null)
+    {
+        if (!empty($args)) {
+            $url .= (stripos($url, '?') !== false) ? '&' : '?';
+            $url .= (is_string($args)) ? $args : http_build_query($args);
+        }
+
+        return $this->request('get', $url);
+    }
+
+    /**
+     * HTTP POST request
+     * @param  string $url
+     * @param  array  $data
+     * @return Response
+     */
+    public function post($url, array $data = [])
+    {
+        return $this->request('post', $url, $data);
+    }
 
 }
